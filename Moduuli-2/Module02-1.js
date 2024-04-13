13d86d81ab2cbfd8f716ebdd67e1b8695a5dbeb4
Tobiaha <122260640+Tobiaha@users.noreply.github.com>
2024-04-13 11:21:07 +0300

    Add files via upload

--- Moduuli-2/Module02-1.js
+++ Moduuli-2/Module02-1.js
@@ -1,1 +1,13 @@
+'use strict'
+let numbers = [];
+
+//5 numbers from prompt
+for (let i = 0; i < 5; i++) {
+  let numb = parseInt(prompt(`Give five numbers, number ${i + 1}:`));
+  numbers.push(numb);
+}
+//numbers.length
+for (let i = numbers.length - 1; i >= 0; i--) {
+  console.log("numbers in reverse: ", numbers[i]); //result in reverse order
+}
 
